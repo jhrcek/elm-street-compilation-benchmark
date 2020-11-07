5722a568d5ef1a8b66aa0855aada40d67fc8c8d6
Jan Hrček <honza.hrk@gmail.com>
2020-11-07 08:23:37 +0100

    Remove leftover files

--- src/Record1.hs
+++
@@ -1,7 +0,0 @@
-{-# LANGUAGE DeriveGeneric, DerivingVia #-}
-module Record1 where
-import Data.Aeson (FromJSON, ToJSON)
-import Elm (Elm, ElmStreet (ElmStreet))
-import GHC.Generics (Generic)
-
-data B1 = B1{ myUnit1::(), myInt1::Int, myDouble1::Double, myFloat1::Float, myBool1::Bool, myList1::[Int], myString1::String } deriving stock Generic deriving (Elm, ToJSON, FromJSON) via ElmStreet B1
--- src/Record2.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE DeriveGeneric, DerivingVia #-}
-module Record2 where
-import Data.Aeson (FromJSON, ToJSON)
-import Elm (Elm, ElmStreet (ElmStreet))
-import GHC.Generics (Generic)
-
-data B1 = B1{ myUnit1::(), myInt1::Int, myDouble1::Double, myFloat1::Float, myBool1::Bool, myList1::[Int], myString1::String } deriving stock Generic deriving (Elm, ToJSON, FromJSON) via ElmStreet B1
-data B2 = B2{ myUnit2::(), myInt2::Int, myDouble2::Double, myFloat2::Float, myBool2::Bool, myList2::[Int], myString2::String } deriving stock Generic deriving (Elm, ToJSON, FromJSON) via ElmStreet B2
